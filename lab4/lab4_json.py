402ea8f394cff4d0544c4e430bcb70f5e219668a
AssylbekZhumash <asyylbekjumashh@gmail.com>
2025-02-15 05:52:54 +0000

    text to commit

--- lab4/lab4_json.py
+++ lab4/lab4_json.py
@@ -1,26 +1,26 @@
 import json
 import os
 
-# Определяем путь к файлу
+
 file_path = "lab4/sample-data.json"
 
 
-# Проверяем, существует ли файл
+
 if not os.path.exists(file_path):
     print(f"Ошибка: Файл {file_path} не найден!")
     exit()
 
-# Открываем JSON-файл и загружаем данные
+
 with open(file_path, "r", encoding="utf-8") as file:
     data = json.load(file)
 
-# Заголовок таблицы
+
 print("Interface Status")
 print("=" * 80)
 print(f"{'DN':50} {'Speed':10} {'MTU':10}")
 print("-" * 80)
 
-# Извлекаем интерфейсы и их параметры
+
 for item in data.get("imdata", []):
     attributes = item.get("l1PhysIf", {}).get("attributes", {})
     dn = attributes.get("dn", "N/A")
