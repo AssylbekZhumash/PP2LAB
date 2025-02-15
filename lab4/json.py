bad3aecfe497cc39f7f255d002596a1ae8a65945
AssylbekZhumash <asyylbekjumashh@gmail.com>
2025-02-15 05:40:41 +0000

    text to commit

--- lab4/json.py
+++ lab4/json.py
@@ -1,15 +1,21 @@
 import json
+import os
 
-with open("sample-data.json", "r") as file:
+# Определяем путь к файлу
+desktop = os.path.join(os.path.expanduser("~"), "Desktop")
+file_path = os.path.join(desktop, "lab4", "sample-data.json")
+
+# Открываем JSON-файл и загружаем данные
+with open(file_path, "r", encoding="utf-8") as file:
     data = json.load(file)
 
-
+# Заголовок таблицы
 print("Interface Status")
 print("=" * 80)
 print(f"{'DN':50} {'Speed':10} {'MTU':10}")
 print("-" * 80)
 
-
+# Извлекаем интерфейсы и их параметры
 for item in data["imdata"]:
     dn = item["l1PhysIf"]["attributes"]["dn"]
     speed = item["l1PhysIf"]["attributes"].get("speed", "inherit")
---
+++ lab4/scope.py
@@ -0,0 +1,45 @@
+def myfunc():
+  x = 300
+  print(x)
+
+myfunc()
+
+
+
+# def myfunc():
+#   x = 300
+#   def myinnerfunc():
+#     print(x)
+#   myinnerfunc()
+
+# myfunc()
+
+# x = 300
+
+# def myfunc():
+#   x = 200
+#   print(x)
+
+# myfunc()
+
+# print(x)
+# global keyword
+# def myfunc():
+#   global x
+#   x = 300
+
+# myfunc()
+
+# print(x)
+
+# nonlocal keyword
+
+# def myfunc1():
+#   x = "Jane"
+#   def myfunc2():
+#     nonlocal x
+#     x = "hello"
+#   myfunc2()
+#   return x
+
+# print(myfunc1())
